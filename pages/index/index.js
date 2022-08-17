ed82aad83cbaf9cb09b13b9db349047ceafbccd7
TGDXAT <2811579869@qq.com>
2022-08-17 13:02:29 +0800

    update api

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,4 +1,5 @@
 // pages/index/index.js
+const app = getApp()
 Page({
 
   /**
@@ -9,79 +10,23 @@ Page({
     name:'NAME'
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad(options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
-  },
-
-  getMyInfo(e){
+  getMyInfo(e) {
+    
     //e=event，微信的事件系统，可以将它视为一个微信的内部请求，具体看这里
     //https://developers.weixin.qq.com/miniprogram/dev/framework/view/wxml/event.html
 
-    //此功能已不再可用，将返回匿名信息 2022/8/17
-    //https://developers.weixin.qq.com/community/develop/doc/000e881c7046a8fa1f4d464105b001
-    //https://developers.weixin.qq.com/miniprogram/dev/component/open-data.html
-  
-    let info = e.detail.userInfo;
-    this.setData({
-      src:info.avatarUrl,
-      name:info.nickName
+    // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，
+    // 开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
+    wx.getUserProfile({
+      desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+      success: (res) => {
+        //console.log(res)
+        this.setData({
+          src: res.userInfo.avatarUrl,
+          name: res.userInfo.nickName
+        })
+      }
     })
-
-    //debug
-    //console.log(e);
-    //console.log(this.src);
-    //console.log(this.name);
   }
+
 })
